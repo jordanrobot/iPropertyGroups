5e0f6aa3d2c16b40019592e4030bbc2bf165158c
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-02-25 23:17:15 -0500

    test: add basic integration test for PropertyGroup

---
+++ tests/PropertyGroup.integration.vb
@@ -0,0 +1,16 @@
+﻿AddReference "iPropertyGroups.dll"
+Imports System.Collections.Generic
+Imports iPropertyGroups
+
+Dim props As Dictionary(Of String, String) = New Dictionary(Of String, String) From {
+	{"name1", "value1" },
+	{"name2", "value2"},
+	{"variableThing", "valuesssss"},
+	{"Title", "stuff stuff!"}}
+
+props.Add("Mirror", "=<Title>")
+
+
+Dim oGroup As PropertyGroup = New PropertyGroup("", props)
+Dim oDoc As Inventor.Document = ThisApplication.ActiveDocument
+oGroup.ApplyToAndOverwrite(oDoc)
